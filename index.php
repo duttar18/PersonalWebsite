9827d6a8fabb008dc8f6431083d34e4f85eead07
Raul Dutta <duttar@umich.edu>
2019-08-06 18:22:35 -0400

    created a rough string like net

--- index.php
+++ index.php
@@ -6,16 +6,15 @@
     <title>Raul's Page</title>
     <link rel="stylesheet" href="main.css">
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-    
 </head>
 
 <?php require_once "header.php"; ?>
 
 <body>
-    <canvas id="titleCanvas"></canvas>
-    <script src="canvas.js"></script>
+    <canvas id="title"></canvas>
+    <script src="title.js"></script>
     <footer>
-        Coy right footer 
+        Copy right footer 
     </footer>
 </body>
 
