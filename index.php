9eeac2c418a68d7666abc2377cc59a52815dfeeb
Raul Dutta <duttar@umich.edu>
2019-08-06 00:12:07 -0400

    canvas color edit

--- index.php
+++ index.php
@@ -6,14 +6,14 @@
     <title>Raul's Page</title>
     <link rel="stylesheet" href="main.css">
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-    <script src="canvas.js"></script>
+    
 </head>
 
 <?php require_once "header.php"; ?>
 
 <body>
     <canvas id="titleCanvas"></canvas>
-
+    <script src="canvas.js"></script>
     <footer>
         Coy right footer 
     </footer>
