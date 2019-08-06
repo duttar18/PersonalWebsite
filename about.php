33f08f630cc9d105ca7d190e68cc1fed5198a3b6
Raul Dutta <duttar@umich.edu>
2019-08-05 21:40:34 -0400

    created basics for website

---
+++ about.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+
+
+
+<head>
+    <meta charset="UTF-8">
+    <title>About</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<nav id = navbar>
+    <ul>
+        <li><a href="ho">Home</a></li>
+        <li><a href="#"></a>Home1</a></li>
+        <li><a href="#"></a>Home2</a></li>
+    </ul>
+</nav>
+
+<body>
+    <header>
+        <h1>Rauls site</h1>
+    </header>
+
+    <section>
+        <h1>Title of article</h1>
+        
+    </section>
+
+    <footer>
+        Coy right footer 
+    </footer>
+</body>
+
+</html>
