1c4a0ad01573628c60661d4f4f35401b14166783
Raul Dutta <duttar@umich.edu>
2019-08-05 22:24:36 -0400

    setting index as home

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <nav id = navbar>
     <ul>
-        <li><a href="home.php">Home</a></li>
+        <li><a href="index.php">Home</a></li>
         <li><a href="about.php"></a>About</a></li>
         <li><a href="projects.php"></a>Projects</a></li>
     </ul>
