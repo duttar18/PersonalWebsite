b236189386f23bb8aaa97761270217d50c9627c8
Raul Dutta <duttar@umich.edu>
2019-08-05 23:58:53 -0400

    canvas animation test

--- index.php
+++ index.php
@@ -6,19 +6,13 @@
     <title>Raul's Page</title>
     <link rel="stylesheet" href="main.css">
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="canvas.js"></script>
 </head>
 
 <?php require_once "header.php"; ?>
 
 <body>
-    <header>
-        <h1>Rauls site</h1>
-    </header>
-
-    <section>
-        <h1>Title of article</h1>
-        
-    </section>
+    <canvas id="titleCanvas"></canvas>
 
     <footer>
         Coy right footer 
